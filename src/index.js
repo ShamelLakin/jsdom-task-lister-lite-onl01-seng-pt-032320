44acbc04cd31c81ae37ada1c37ac134971f3b8d6
Shamel Lakin <christlife4u@gmail.com>
2020-09-30 19:25:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
+const main = document.querySelector("#main-content")
+console.log(main)
+
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+
+  const newTask = document.querySelector('#new-task-description').value
+
+  taskList.innerHTML += `
+  <li> ${newTask}
+  <button data-action="delete">X for delete</button>
+  </li>
+  
+  `
+
+  // const taskItem = document.createElement("li")
+  // taskItem.innerHTML = newTask
+
+  // taskList.appendChild(taskItem)
+  taskForm.reset()
+
+})
+
+taskList.addEventListener("click", function(e) {
+console.log(e.target)
+if (e.target.dataset.action === "delete") {
+  e.target.parentElement.remove()
+  }
+})
